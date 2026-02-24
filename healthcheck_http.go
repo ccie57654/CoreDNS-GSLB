cf647ff96a31ac0c0d447fd1956450fb725fb70f
ccie57654 <ccie57654@none.empty>
2026-02-23 16:06:40 -0800

    added tls config functions

--- healthcheck_http.go
+++ healthcheck_http.go
@@ -3,6 +3,7 @@ package gslb
 import (
 	"context"
 	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 	"io"
 	"net"
@@ -14,6 +15,22 @@ import (
 
 	"github.com/creasty/defaults"
 )
+
+func setTLSDefaults(ctls *tls.Config) {
+	ctls.MinVersion = tls.VersionTLS12
+	ctls.MaxVersion = tls.VersionTLS13
+	ctls.CipherSuites = []uint16{
+		tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+		tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+		tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+		tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+		tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+		tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+		tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+		tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+		tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+	}
+}
 
 // HTTPHealthCheck represents HTTP-specific health check settings.
 type HTTPHealthCheck struct {
@@ -52,6 +69,8 @@ func createHTTPClient(enableTLS bool, skipTLSVerify bool, timeout time.Duration)
 	var tlsConfig *tls.Config
 	if enableTLS {
 		tlsConfig = &tls.Config{
+            RootCAs:            caCertPool,
+            Certificates:       []tls.Certificate{tlsCertificate},
 			InsecureSkipVerify: skipTLSVerify,
 		}
 	}
@@ -220,6 +239,64 @@ func (h *HTTPHealthCheck) Equals(other GenericHealthCheck) bool {
 	}
 
 	return true
+}
+
+// NewTLSConfig returns a TLS config that includes a certificate
+// Use for server TLS config or when using a client certificate
+// If caPath is empty, system CAs will be used
+func NewTLSConfig(certPath, keyPath, caPath string) (*tls.Config, error) {
+	cert, err := tls.LoadX509KeyPair(certPath, keyPath)
+	if err != nil {
+		return nil, fmt.Errorf("could not load TLS cert: %s", err)
+	}
+
+	roots, err := loadRoots(caPath)
+	if err != nil {
+		return nil, err
+	}
+
+	// #gosec G402 -- MinVersion and MaxVersion are set in setTLSDefaults
+	tlsConfig := &tls.Config{
+		Certificates: []tls.Certificate{cert},
+		RootCAs:      roots,
+	}
+	setTLSDefaults(tlsConfig)
+
+	return tlsConfig, nil
+}
+
+// NewTLSClientConfig returns a TLS config for a client connection
+// If caPath is empty, system CAs will be used
+func NewTLSClientConfig(caPath string) (*tls.Config, error) {
+	roots, err := loadRoots(caPath)
+	if err != nil {
+		return nil, err
+	}
+
+	// #nosec G402 -- MinVersion and MaxVersion are set in setTLSDefaults
+	tlsConfig := &tls.Config{
+		RootCAs: roots,
+	}
+	setTLSDefaults(tlsConfig)
+
+	return tlsConfig, nil
+}
+
+func loadRoots(caPath string) (*x509.CertPool, error) {
+	if caPath == "" {
+		return nil, nil
+	}
+
+	roots := x509.NewCertPool()
+	pem, err := os.ReadFile(filepath.Clean(caPath))
+	if err != nil {
+		return nil, fmt.Errorf("error reading %s: %s", caPath, err)
+	}
+	ok := roots.AppendCertsFromPEM(pem)
+	if !ok {
+		return nil, fmt.Errorf("could not read root certs: %s", err)
+	}
+	return roots, nil
 }
 
 // Helper function to build the health check URL
