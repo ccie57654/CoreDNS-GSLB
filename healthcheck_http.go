b87cf3ba24bf93f7a4754c220f87b13445dc08e8
ccie57654 <ccie57654@proton.me>
2026-05-14 19:54:57 -0700

    remove defaults

--- healthcheck_http.go
+++ healthcheck_http.go
@@ -18,22 +18,6 @@ import (
 	"github.com/creasty/defaults"
 )
 
-func setTLSDefaults(ctls *tls.Config) {
-	ctls.MinVersion = tls.VersionTLS12
-	ctls.MaxVersion = tls.VersionTLS13
-	ctls.CipherSuites = []uint16{
-		tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-		tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-		tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
-		tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
-		tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-		tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-		tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-		tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-		tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-	}
-}
-
 // HTTPHealthCheck represents HTTP-specific health check settings.
 type HTTPHealthCheck struct {
 	Port          int               `yaml:"port" default:"443"`
